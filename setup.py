a329936dc2fdfb2327ce2ed39d56c85034314f7a
Peter Ward <peteraward@gmail.com>
2012-11-16 11:32:01 +1100

    add metadata

--- setup.py
+++ setup.py
@@ -3,11 +3,21 @@ from setuptools import find_packages, setup
 setup(
     name='pjlink',
     version='1.0',
-    description='PJLink is a standard for controlling data projectors.',
     author='Peter Ward',
     author_email='peteraward@gmail.com',
-    packages=find_packages(),
+    url='http://hg.flowblok.id.au/pjlink',
+    description='PJLink is a standard for controlling data projectors.',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Topic :: Multimedia :: Video :: Display',
+        'Topic :: Utilities',
+    ],
+
     install_requires=['appdirs'],
+    packages=find_packages(),
     entry_points = {
         'console_scripts': [
             'pjlink = pjlink.cli:main',
